a86e6fb97db378d75ee38b4eae44bd491afcfad4
Sébastien DEVAUX <devsebastien@hotmail.fr>
2018-01-31 23:29:19 +0100

    [ADD] Update

--- app/controllers/corsairs_controller.rb
+++ app/controllers/corsairs_controller.rb
@@ -1,13 +1,15 @@
 class CorsairsController < ApplicationController
 
   def new
+    @moussaillon = Corsair.new
   end
 
   def create
-    @moussaillon = Corsair.new    #(corsair_params)
+    @moussaillon = Corsair.new(params[:corsair])  #(corsair_params)
     @moussaillon.first_name = params[:first_name]
     @moussaillon.age = params[:age]
     @moussaillon.save
+    p params
 
     redirect_to @moussaillon
 
@@ -17,9 +19,28 @@ class CorsairsController < ApplicationController
     @moussaillon = Corsair.find(params[:id])
   end
 
-  private
+  def edit
+    @moussaillon = Corsair.find(params[:id])
+  end
 
-  # def corsair_params
-  #   params.require(:corsair).permit(:first_name, :age)
-  # end
+  def update
+    @moussaillon = Corsair.find(params[:id])
+
+    if @moussaillon.update(corsair_params) #(corsair_params)
+      redirect_to @moussaillon
+    else
+      render 'edit'
+    end
+  end
+
+  def index
+    @moussaillons = Corsair.all
+  end
+
+  # private
+
+  def corsair_params
+    params.require(:corsair).permit(:first_name, :age) # :likeness, :bio)
+  end
+
 end
